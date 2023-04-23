48790855c402dc53564933191e43ff433d674430
jovialcore <jollydosky@gmail.com>
2023-04-23 15:11:10 +0100

    installed fastoutes, laminas diactoros (http message client, relay(router dispatcher) and middlewares for these routers that correspond with psr standards

--- public/index.php
+++ public/index.php
@@ -5,22 +5,24 @@ declare(strict_types=1);
 
 use DI\ContainerBuilder;
 use Controller\Aicontroller;
+use FastRoute\RouteCollector;
+use Relay\Relay;
+use Laminas\Diactoros\ServerRequestFactory; // more like guzzlehttp client
+use FastRoute\SimpleDispatcher;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
 
 use function Di\create;
+use function FastRoute\simpleDispatcher;
 
 require_once dirname(__DIR__) . '/vendor/autoload';
-
-
-$aicontroller  = new Aicontroller();
-$aicontroller->extractCodeText();
-
 
 
 $containerBuilder = new  ContainerBuilder();
 $containerBuilder->useAutowiring(false);
 //$containerBuilder->useAnnotations(false); // annotations are now disabled by default no need to add them
 
-/// add definitions
+/// add definitions // register classes so that the DI can automatically create instances of those class in whatever situtatuion (contructor, methods injections, etc) they are called
 $containerBuilder->addDefinitions(
     [
         Aicontroller::class => create(Aicontroller::class)
@@ -29,3 +31,16 @@ $containerBuilder->addDefinitions(
 $container = $containerBuilder->build();
 // let Di create the objects aka instantiate
 $extractText = $container->get(\Controller\Aicontroller::class);
+
+
+// defining the routes
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->get('/api/extract/imagetext', Aicontroller::class);
+});
+
+// the different layers the request coming in has to pass before it gets to the application
+
+$middleware[] = new FastRoute($routes);
+$middleware[] = new RequestHandler();
+$requestHandler = new Relay($middleware);
+$requestHandler->handle(ServerRequestFactory::fromGlobals());
