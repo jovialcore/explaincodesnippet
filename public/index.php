29e68d05aa449c804e2300502225d3d6857216c0
jovialcore <jollydosky@gmail.com>
2023-04-25 15:48:05 +0100

    the DI container that worked

--- public/index.php
+++ public/index.php
@@ -2,44 +2,55 @@
 
 declare(strict_types=1);
 
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use DI\ContainerBuilder;
-use ExplainImgSnippetAi\AiController;
+use ExplainImgSnippetAi\genesis;
 use FastRoute\RouteCollector;
-use Relay\Relay;
-use Laminas\Diactoros\ServerRequestFactory; // more like guzzlehttp client
-use FastRoute\SimpleDispatcher;
 use Middlewares\FastRoute;
 use Middlewares\RequestHandler;
 
+use Relay\Relay;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequestFactory;
+use Narrowspark\HttpEmitter\SapiEmitter;
+
+use function FastRoute\SimpleDispatcher;
+
 use function Di\create;
-use function FastRoute\simpleDispatcher;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-$containerBuilder = new  ContainerBuilder();
+//dependency injection but using the libray php-di
+$containerBuilder = new ContainerBuilder();
 $containerBuilder->useAutowiring(false);
 //$containerBuilder->useAnnotations(false); // annotations are now disabled by default no need to add them
+$containerBuilder->addDefinitions([
 
-/// add definitions // register classes so that the DI can automatically create instances of those class in whatever situtatuion (contructor, methods injections, etc) they are called
-$containerBuilder->addDefinitions(
-    [
-        AiController::class => create(AiController::class)
-    ]
-);
+    // handles all dependency and injection
+    genesis::class => create(genesis::class)->constructor('People and my '),
+]);
+
+// understanding namespaces in php https://www.php.net/manual/en/language.namespaces.rationale.php#116280
+
+//what does the container Builder or libray do: it automatically injects a dependecny for your class parameter so you don't have to instantiate the classes (not like the class is not being instantiated but you will not need to use new Keyword, etc)
 $container = $containerBuilder->build();
-// let Di create the objects aka instantiate
-$extractText = $container->get(AiController::class);
 
-
-// defining the routes
-$routes = simpleDispatcher(function (RouteCollector $r) {
-    $r->get('/api/extract/imagetext', AiController::class);
+// the route
+$routes = SimpleDispatcher(function (RouteCollector  $route) {
+    $route->get('/api', genesis::class);
 });
 
-// the different layers the request coming in has to pass before it gets to the application
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler($container);
 
-$middleware[] = new FastRoute($routes); // middleware/layer to match url patterns
-$middleware[] = new RequestHandler(); // middleware/layer to return response from server
-$requestHandler = new Relay($middleware); //  put all the middlewares in the pipeline
-$requestHandler->handle(ServerRequestFactory::fromGlobals()); // pull all middlewares in the pipeline using the current http method returned
+
+//dispatcher following the psr-15 and psr-7 standards
+$requestHandler = new Relay($middlewareQueue);
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+
+
+
+
+
+//the laminas diactoros package is to implemnet a PSR-7 compatible HTTP Messages
