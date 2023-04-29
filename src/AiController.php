d4a657c2a152714339a97197576488cf60949c76
jovialcore <jollydosky@gmail.com>
2023-04-29 18:46:52 +0100

    playing aorund with tesseractOCR and gd image with imagetickk in php

--- src/AiController.php
+++ src/AiController.php
@@ -7,6 +7,8 @@ namespace ExplainImgSnippetAi;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Orhanerday\OpenAi\OpenAi;
+use thiagoalessio\TesseractOCR\TesseractOCR;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class AiController
 {
@@ -24,6 +26,18 @@ class AiController
     public function api(): ResponseInterface
     {
         $data =   ['message' => "Hello, {$this->chatgptapikey} world!"];
+
+
+
+        // set the path to the image file
+
+        $image_file = 'code.png';
+        $dir_name = dirname(__FILE__);
+        $image = $dir_name . '/' . $image_file;
+
+
+        $image = Image::make('path/to/image.jpg');
+
 
         $openaikey = getenv('OPENAI_API_KEY');
 
