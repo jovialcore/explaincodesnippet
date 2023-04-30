7c2885f69cf7f9f3349a30f21370fdb9fd7bb3f3
jovialcore <jollydosky@gmail.com>
2023-04-30 17:10:24 +0100

    aws is not awsing

--- src/AiController.php
+++ src/AiController.php
@@ -7,6 +7,8 @@ namespace ExplainImgSnippetAi;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Orhanerday\OpenAi\OpenAi;
+use Aws\Textract\TextractClient;
+use Aws\Textract\Exception\TextractException;
 
 class AiController
 {
@@ -19,11 +21,36 @@ class AiController
         $this->chatgptapikey = $chatgptapikey;
     }
 
-
-
     public function api(): ResponseInterface
     {
+        $textractClient = new TextractClient([
+            'version' => 'latest',
+            'region' => getenv('AWS_REGION'),
+            'credentials' => [
+                'key'    => getenv('AWS_KEY'),
+                'secret' => getenv('AWS_SECRET')
+            ]
+        ]);
         $data =   ['message' => "Hello, {$this->chatgptapikey} world!"];
+
+        try {
+            $result = $textractClient->detectDocumentText([
+                'Document' => [
+                    'Bytes' => file_get_contents('/opt/lampp/htdocs/Projects/explain-img-snippet-ai/src/code.png')
+                ]
+            ]);
+
+            foreach ($result->get('Blocks') as $block) {
+                if ($block['BlockType'] != 'WORD') {
+                    continue;
+                }
+
+                echo $block['Text'] . " ";
+            }
+        } catch (TextractException $e) {
+            // output error message if fails
+            echo $e->getMessage();
+        }
 
         $openaikey = getenv('OPENAI_API_KEY');
 
